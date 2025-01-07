b187576c5746a0816f577cefe1b041801d602f18
sky0427 <sukeun95@gmail.com>
2025-01-07 14:22:42 +0900

    first commit

---
+++ src/test/java/com/ms/Jpa2501/Jpa2501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.Jpa2501;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa2501ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
